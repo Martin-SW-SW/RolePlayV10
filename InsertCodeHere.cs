79409f731c2524fac38a0316f0a80e2b3431b54e
Martin-SW-SW <62882502+Martin-SW-SW@users.noreply.github.com>
2020-03-30 13:26:30 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RolePlayV10
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            Random RNG = new Random();
+            Warrior warriorA = new Warrior("Ragnar", 100 + RNG.Next(50));
+            Warrior warriorB = new Warrior("Lagertha", 100);
+
+            Console.WriteLine("Press a Key to Start"); Console.ReadKey();
+                Console.WriteLine("\n");
+            Console.WriteLine($"Warrior A is called {warriorA.Name + " Health = " + warriorA.Start_Hitpoints}");
+            Console.WriteLine($"Warrior B is called {warriorB.Name + " Health = " + warriorB.Start_Hitpoints}");
+                Console.WriteLine("\n");
+            
+            //Levelup Tester
+            Console.WriteLine("Press a Key to Start LevelUP" + "\n"); Console.ReadKey();
+            Console.WriteLine(warriorA.level);
+                warriorA.levelUP(); Console.ReadKey();
+            Console.WriteLine(warriorA.level);
+                warriorA.levelUP(); Console.ReadKey();
+            Console.WriteLine(warriorA.level);
+                warriorA.levelUP(); Console.ReadKey();
+            Console.WriteLine(warriorA.level);
+                warriorA.levelUP(); Console.ReadKey();
+
+
+            //DMG Tester
+                Console.WriteLine("\n");
+            Console.WriteLine($"Warrior A TEST {warriorA.Name + " DMG = " + warriorA.DealDMG()}");
+            Console.WriteLine($"Warrior B TEST {warriorB.Name + " DMG = " + warriorB.DealDMG()}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
---
+++ Warrior.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace RolePlayV10
+{
+    public class Warrior
+    {
+        private string _name;
+        private int _level;
+        private bool _Death;
+        private int _DealDMG;
+        private int _Start_Hitpoints;
+
+        public Warrior(string name, int Start_Hitpoints)
+        {
+            _name = name;
+            _level = 1;
+            _Start_Hitpoints = Start_Hitpoints;
+            
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+        
+        public int level
+        {
+              get { return _level; }
+        }
+
+         public int Start_Hitpoints
+        {
+              get { return _Start_Hitpoints; }
+        }
+
+        public void levelUP()
+        {
+            _level = _level + 1;
+
+        }
+        public bool Death
+        {
+            get { return Start_Hitpoints <= 0; }
+        }
+    
+        public int DealDMG()
+        {
+        Random RNG = new Random();
+          _DealDMG = RNG.Next(10);
+          return _DealDMG;
+        }
+
+    }
+}
